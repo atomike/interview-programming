8a4a71cce3db70770df41cd613dcc4e14e3246ce
Michael Diez <mdiez@m10digital.com>
2020-04-09 18:49:02 -0400

    added example to sprintf.php

--- PHP/functions/sprintf.php
+++ PHP/functions/sprintf.php
@@ -12,4 +12,9 @@ echo "\n\n";
 
 echo "Integer 3 with 2 decimal padding\n";
 echo sprintf("%.2f", 3);
+
+echo "\n\n";
+
+echo "Any number with three zeroes at the end\n";
+echo sprintf("%d000", 48);
 ?>
