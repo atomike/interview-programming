089b8bd415946ebc8d2e7fb37ff8351f43e0473b
Michael Diez <mdiez@m10digital.com>
2020-05-02 16:11:30 -0400

    range PHP function

---
+++ PHP/functions/range.php
@@ -0,0 +1,12 @@
+<?php
+
+var_dump(
+    range(0, 10),
+    range(10, 100, 10),
+    range("a", "z"),
+    range("A", "z"),
+    range("AA", "Z"),
+    range(0, 10, 0.1),
+    range(20, "a"),
+);
+?>
